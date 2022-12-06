b954941b7da4a3be81baf60020767b0ae79485c9
martijnvdp <m.vanderploeg@nl.ccv.eu>
2022-12-06 19:57:23 +0100

    kubespray

--- kubespray/getKubeconfig.sh
+++ kubespray/getKubeconfig.sh
@@ -1,4 +1,4 @@
-scp $USERNAME@$IP_CONTROLLER_0:/etc/kubernetes/admin.conf kubespray-do.conf
 read -p "enter controller node ip: " controlIP
+scp ubuntu@$controlIP:/etc/kubernetes/admin.conf kubespray-do.conf
 sed -i s/127.0.0.1:6443/$controlIP:6443/g kubespray-do.conf
 export KUBECONFIG=$PWD/kubespray-do.conf
