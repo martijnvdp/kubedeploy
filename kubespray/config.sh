cb20e1fdc209406c2a1d37f2f64522bb0cb74562
martijnvdp <m.vanderploeg@nl.ccv.eu>
2022-12-06 18:13:30 +0100

    inventory kubespray

--- kubespray/config.sh
+++ kubespray/config.sh
@@ -1,4 +1,4 @@
-sed -i -r 's/^(container_manager:).*/\1 containerd/g' inventory/sample/group_vars/k8s_cluster/k8s-cluster.yml
-sed -i -r 's/^(kube_network_plugin:).*/\1 cni/g' inventory/sample/group_vars/k8s_cluster/k8s-cluster.yml
-sed -i -r 's/^(etcd_deployment_type:).*/\1 host/g' inventory/sample/group_vars/etcd.yml
-echo "kube_proxy_remove: true" >> inventory/sample/group_vars/k8s_cluster/k8s-cluster.yml
+sed -i -r 's/^(container_manager:).*/\1 containerd/g' inventory/mycluster/group_vars/k8s_cluster/k8s-cluster.yml
+sed -i -r 's/^(kube_network_plugin:).*/\1 cni/g' inventory/mycluster/group_vars/k8s_cluster/k8s-cluster.yml
+sed -i -r 's/^(etcd_deployment_type:).*/\1 host/g' inventory/mycluster/group_vars/etcd.yml
+echo "kube_proxy_remove: true" >> inventory/mycluster/group_vars/k8s_cluster/k8s-cluster.yml
--- kubespray/reset.sh
+++ kubespray/reset.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i mycluster/hosts.yaml  --become --become-user=root kubespray/reset.yml
+ansible-playbook -i inventory/mycluster/hosts.yaml  --become --become-user=root kubespray/reset.yml
