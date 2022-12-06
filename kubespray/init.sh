226a8b144e0dec889c78bbf0a5dab0b24fee2359
martijnvdp <m.vanderploeg@nl.ccv.eu>
2022-12-06 19:17:12 +0100

    extention

--- kubespray/init.sh
+++ kubespray/init.sh
@@ -3,4 +3,4 @@ mkdir inventory
 cp -rfp kubespray/inventory/sample inventory/mycluster
 read -p "node ips (space seperated) ex 1.2.3.4 1.2.3.5:" nodeIPS
 declare -a IPS=($nodeIPS)
-CONFIG_FILE=inventory/mycluster/hosts.yml python3 kubespray/contrib/inventory_builder/inventory.py ${IPS[@]}
+CONFIG_FILE=inventory/mycluster/hosts.yaml python3 kubespray/contrib/inventory_builder/inventory.py ${IPS[@]}
